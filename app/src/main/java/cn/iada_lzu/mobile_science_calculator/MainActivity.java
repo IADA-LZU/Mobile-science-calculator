7f190dab6276b01ecfe1fe4fa3b08a115c203e74
hopeful0 <hopeful_world@163.com>
2016-04-20 11:39:35 +0800

    use new UI

--- app/src/main/java/cn/iada_lzu/mobile_science_calculator/MainActivity.java
+++ app/src/main/java/cn/iada_lzu/mobile_science_calculator/MainActivity.java
@@ -1,78 +1,147 @@
 package cn.iada_lzu.mobile_science_calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
-import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener{
 
-    ClientSocket socket;
+public class MainActivity extends FragmentActivity implements OnClickListener {
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter mAdapter;
+    private List<Fragment> mFragments;
+
+    private LinearLayout mTabMatrix;
+    private LinearLayout mTabPoly;
+
+    private TextView MatrixText;
+    private TextView PolyText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.bt1).setOnClickListener(this);
+
+        initView();
+        initEvent();
+
+        setSelect(0);
+    }
+
+    private void initEvent() {
+        mTabMatrix.setOnClickListener(this);
+        mTabPoly.setOnClickListener(this);
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+
+        mTabMatrix = (LinearLayout) findViewById(R.id.id_tab_matrix);
+        mTabPoly = (LinearLayout) findViewById(R.id.id_tab_polynomial);
+
+        MatrixText = (TextView) findViewById(R.id.matrix_text);
+        PolyText = (TextView) findViewById(R.id.polynomial_text);
+
+        mFragments = new ArrayList<Fragment>();
+        Fragment mTab01 = new MatrixFragment();
+        Fragment mTab02 = new PolyFragment();
+
+        mFragments.add(mTab01);
+        mFragments.add(mTab02);
+
+
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+
+            @Override
+            public int getCount() {
+                return mFragments.size();
+            }
+
+            @Override
+            public Fragment getItem(int arg0) {
+                return mFragments.get(arg0);
+            }
+        };
+        mViewPager.setAdapter(mAdapter);
+
+        mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int arg0) {
+                int currentItem = mViewPager.getCurrentItem();
+                setTab(currentItem);
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void onClick(View v) {
-        switch (v.getId())
-        {
-            case R.id.bt1:
-                socket = new ClientSocket();
-                findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
-                socket.requestResult(((EditText)findViewById(R.id.et1)).getText().toString(), new ClientSocket.CallOver() {
-                    @Override
-                    public void onError(final String emsg) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(MainActivity.this,emsg,Toast.LENGTH_SHORT).show();
-                                findViewById(R.id.progressBar).setVisibility(View.GONE);
-                            }
-                        });
-                    }
+        switch (v.getId()) {
+            case R.id.id_tab_matrix:
+                setSelect(0);
+                break;
+            case R.id.id_tab_polynomial:
+                setSelect(1);
+                break;
 
-                    @Override
-                    public void onFinished(final String result) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                ((TextView)findViewById(R.id.tv1)).setText(result);
-                                findViewById(R.id.progressBar).setVisibility(View.GONE);
-                            }
-                        });
-                    }
-                });
+            default:
+                break;
+        }
+    }
+
+    private void setSelect(int i) {
+        setTab(i);
+        mViewPager.setCurrentItem(i);
+    }
+
+    private void setTab(int i)
+    {
+        resetImgs();
+        // 设置图片为亮色
+        // 切换内容区域
+        switch (i)
+        {
+            case 0:
+                MatrixText.setTextColor(this.getResources().getColor(R.color.blue));
+                break;
+            case 1:
+                PolyText.setTextColor(this.getResources().getColor(R.color.blue));
                 break;
 
         }
     }
+
+    /**
+     * 切换图片至暗色
+     */
+    private void resetImgs() {
+        MatrixText.setTextColor(this.getResources().getColor(R.color.half_transparent));
+        PolyText.setTextColor(this.getResources().getColor(R.color.half_transparent));
+    }
+
 }
